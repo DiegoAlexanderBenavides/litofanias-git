6a1e78e27b6d99f3c7e8ef60de8f2ae4fa17d9bb
Diego Alexander Benavides Montenegro <diegoven@unicauca.edu.co>
2023-02-16 17:58:23 -0500

    terminando clase 4

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+( ()=>{
 const btn= document.querySelector("[data-form-btn]");
 
 const createtask=(evento)=>{
@@ -8,15 +9,40 @@ const createtask=(evento)=>{
     input.value='';
     const task=document.createElement('li');
     task.classList.add('card')
-    const content=`<div>
-    <i class="far fa-check-square icon"></i>
+    const taskcontent=document.createElement("div");
+    taskcontent.appendChild(checkComplete());
+    const titleTask=document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerHTML=value;
+    taskcontent.appendChild(titleTask);
+
+    const content=`
+    ${checkComplete}
     <span class="task">${value}</span>
-  </div>
+  
   <i class="fas fa-trash-alt trashIcon icon"></i>`
-  task.innerHTML=content;
+  //task.innerHTML=content;
+  task.appendChild(taskcontent);
   list.appendChild(task)
+
     console.log(value)
 
 }
 console.log(btn);
 btn.addEventListener('click', createtask);
+const checkComplete=()=>{
+    const i=document.createElement("i");
+    i.classList.add("far",'fa-check-square','icon');
+    i.addEventListener('click', completeTask);
+    return i;
+}
+
+const completeTask=(event)=>{
+    const element=event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+
+    
+}
+})();
